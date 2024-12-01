f72a4494e4c506918d5d7bd593435b6ec06d6134
stumpf-alexandre <als.stumpf@gmail.com>
2024-12-01 18:05:42 -0300

    estilizando a animação 3d do carrossel

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -22,6 +22,17 @@ function addEventListenersCards() {
 /**só libera o script após carregar toda a pagina */
 document.addEventListener("DOMContentLoaded", addEventListenersCards, false);
 
-function selectCarouselItem(selectedItem) {
-    console.log("selecionando item", selectedItem);
+function selectCarouselItem(selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector(".s-cards-carousel");
+    const transforms = carousel.style.transform;
+    const rotateY = transforms.match(/rotateY\((-?\d+deg)\)/i);
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    const newTransform = transforms.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
+
+    carousel.style.transform = newTransform;
+
+    const activeButtonElement = document.querySelector(".s-controller__button--active");
+    activeButtonElement.classList.remove("s-controller__button--active");
+    selectedButtonElement.classList.add("s-controller__button--active");
 }
